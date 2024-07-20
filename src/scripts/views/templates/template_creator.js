ebf475bf5e2324e2915a53aa58c13ffd066f002c
Faishal AR <Aswadalansori@gmail.com>
2024-07-21 05:50:56 +0700

    modify lazysizes 🦥

--- src/scripts/views/templates/template_creator.js
+++ src/scripts/views/templates/template_creator.js
@@ -1,8 +1,10 @@
 import CONFIG from '../../globals/config';
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
 const createRestoDetailTemplate = (resto) => `
   <h2 class="restaurant__title"><i class="fa fa-cutlery"></i> Detail Restaurant</h2>
-  <img class="restaurant__poster lazyload" src="${CONFIG.IMAGE_ENDPOINT_LARGE + resto.pictureId}" alt="${resto.name}" />
+  <img class="restaurant__poster lazyload" data-src="${CONFIG.IMAGE_ENDPOINT_LARGE + resto.pictureId}" alt="${resto.name}" />
   <div class="restaurant__info">
     <h4><i class="fa fa-info-circle"></i> Nama Restaurant</h4>
     <p>${resto.name}</p>
@@ -37,8 +39,8 @@ const createRestoDetailTemplate = (resto) => `
 const createRestoItemTemplate = (resto) => `
   <div class="restaurant-item">
     <div class="restaurant-item__header">
-      <img class="restaurant-item__header__poster" alt="${resto.name}"
-           src="${resto.pictureId ? CONFIG.IMAGE_ENDPOINT_LARGE + resto.pictureId : 'https://picsum.photos/id/666/800/450?grayscale'}">
+      <img class="restaurant-item__header__poster lazyload" alt="${resto.name}"
+           data-src="${resto.pictureId ? CONFIG.IMAGE_ENDPOINT_LARGE + resto.pictureId : 'https://picsum.photos/id/666/800/450?grayscale'}">
       <div class="restaurant-item__header__rating">
         <p>⭐️<span class="restaurant-item__header__rating__score">${resto.rating}</span></p>
       </div>
